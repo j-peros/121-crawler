ac25d434fde6faa9ed5abf9e2797bbfc2dee0b68
Tuan Nguyen <tuanvnguyen556@gmail.com>
2023-04-28 20:23:35 -0700

    still need part 3

--- reports.py
+++ reports.py
@@ -4,8 +4,13 @@ from unique import Unique
 
 
 with open("reports.txt", "w") as report:
+    #writing the number of unique pages
     report.write(f"1. The number of unique pages found is: {Unique.url_counter}",)
+    #writing the maximum number of words from the crawled webpages
     report.write(f"2. The maximum number of words overall the webpages crawled is: {maxWordCount.maxWords}\n")
+    #writing the 50 most common words besides stop words
+    report.write(f"3.")
+    #writing the pair of subdomains and number of unique pages detected
     report.write("""4. Here is a list of all the subdomains found in the ics.uci.edu domain in alphabetical order, 
                 along with the number of unique pages detected in each subdomain:\n""")
     for key, value in icsSubdomains.subdomainDict.items():
