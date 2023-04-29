baa1173ca07fdb64de2b34a86f82fa32a39f8a6b
Yash Balaji Pathak <pathaky@circinus-18.ics.uci.edu>
2023-04-29 14:44:09 -0700

    Added longest url to reports

--- reports.py
+++ reports.py
@@ -13,6 +13,7 @@ def create_report() -> None:
         report.write(f"1. The number of unique pages found is: {Unique.url_counter}\n")
         #writing the maximum number of words from the crawled webpages
         report.write(f"2. The maximum number of words overall the webpages crawled is: {maxWordCount.maxWords}\n")
+        report.write(f"And the url with these max words is: {maxWordCount.longestURL}\n.")
         #writing the 50 most common words besides stop words
         report.write(f"3. \n")
         #writing the pair of subdomains and number of unique pages detected
