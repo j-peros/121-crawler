f6670be3fd685ee277bf576352950fb9dc3acb9e
j-peros <jperos@uci.edu>
2023-04-29 17:43:38 -0700

    RobotCheck checks the robots.txt, and adds that (+ sitemaps) to links list

--- scraper.py
+++ scraper.py
@@ -9,6 +9,7 @@ from write_save_files import Counter
 import unique
 from nltk.corpus import stopwords
 import json
+import robotCheck
 
 stop_words = set(stopwords.words('english'))
 word_counter = {}
@@ -19,7 +20,15 @@ delete = 0
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    valid_links = []
+    for link in links:
+        if is_valid(link):
+            domain = urlparse(link).netloc # domain
+            sitemap_links = robotCheck.RobotCheck.get_sitemap(domain)
+            sitemap_links.append(link) # list of sitemaps + next valid link
+            valid_links += sitemap_links
+            # print(link, ":", sitemap_links) # use this to test and see what links are added
+    return valid_links
 
 def extract_next_links(url, resp):
     # Implementation required.
@@ -92,6 +101,8 @@ def is_valid(url):
             return False
         if not re.match('\S*.ics.uci.edu$|\S*.cs.uci.edu$|\S*.informatics.uci.edu$|\S*.stat.uci.edu$', parsed.netloc):
             return False # \S* matches any character before, so we don't have to worry if www is there or not, and $ makes sure the domain ends after that
+        if not robotCheck.RobotCheck.checkURL(parsed.scheme, parsed.netloc, url):
+            return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
